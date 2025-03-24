87e3009c6d2624cdc8864da27406fff5f3a0849f
duda4418 <146849849+duda4418@users.noreply.github.com>
2025-03-24 15:32:35 +0200

    Add UI

---
+++ AirPort-App/UI_Airport-App/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_Airport_App
+{
+    public class Class1
+    {
+    }
+}
